4ccf177c26fc4f26afab473c6dabab588068b65e
Châteauneufsandrine <chateauneufsandrine188@gmail.com>
2026-03-26 11:34:01 +0100

    add2 exercice5

--- exercice5/index.php
+++ exercice5/index.php
@@ -6,6 +6,18 @@ existent et les afficher: index.php?week=12 -->
 <?php 
 
 var_dump($_GET);
-exit();
+
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="index.php?week=12 ">Accueil</a>
+</body>
+</html>
