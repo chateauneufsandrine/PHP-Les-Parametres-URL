771c3edeabdd04f7b961aea9f6549cefe5b29fa2
Châteauneufsandrine <chateauneufsandrine188@gmail.com>
2026-03-26 11:31:51 +0100

    add2 exercice3

--- exercice3/index.php
+++ exercice3/index.php
@@ -5,6 +5,18 @@ existent et les afficher: index.php?startDate=2/05/2016&endDate=27/11/2016 -->
 <?php 
 
 var_dump($_GET);
-exit();
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="index.php?startDate=2/05/2016&endDate=27/11/2016">Accueil</a>
+</body>
+</html>
