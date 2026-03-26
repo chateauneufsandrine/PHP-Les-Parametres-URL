6f892e5569cad65e9ed4735504265bbfc0aac249
Châteauneufsandrine <chateauneufsandrine188@gmail.com>
2026-03-26 11:07:34 +0100

    add exercice5

--- exercice5/index.php
+++ exercice5/index.php
@@ -1,3 +1,11 @@
 <!-- EXERCICE5/////////////////////////////////////////////////////////////////////////////////
 Faire une page index.php. Tester sur cette page que tous les paramètres de cette URL 
 existent et les afficher: index.php?week=12 -->
+
+
+<?php 
+
+var_dump($_GET);
+exit();
+
+?>
