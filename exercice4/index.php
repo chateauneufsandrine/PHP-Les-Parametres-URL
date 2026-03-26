bb7b9ce2c9500062559139922beb9bd3b4458d98
Châteauneufsandrine <chateauneufsandrine188@gmail.com>
2026-03-26 11:06:16 +0100

    add exercice4

--- exercice4/index.php
+++ exercice4/index.php
@@ -1,3 +1,10 @@
 <!-- EXERCICE4///////////////////////////////////////////////////////////
 Faire une page index.php. Tester sur cette page que tous les paramètres de cette URL 
 existent et les afficher: index.php?language=PHP&server=LAMP -->
+
+<?php 
+
+var_dump($_GET);
+exit();
+
+?>
