3cf99a171c0cf07ad58beb6dbf3ae25b61b45f39
Châteauneufsandrine <chateauneufsandrine188@gmail.com>
2026-03-26 10:27:18 +0100

    add exercice1

--- exercice1/index.php
+++ exercice1/index.php
@@ -1,3 +1,25 @@
 <!-- EXERCICE1////////////////////////////////////////////////////////////////////////////////////
 Faire une page index.php. Tester sur cette page que tous les paramètres de cette URL
  existent et les afficher: index.php?lastname=Nemare&firstname=Jean -->
+
+ <?php 
+ 
+
+var_dump($_GET);
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   
+</body>
+</html>
+ 
+ 
---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php 
+var_dump($_GET);
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="index.php?lastname=Nemare&firstname=Jean">aller à la page exercice1 index.php</a>
+</body>
+</html>
