e83a6b1120d6e02738f04ab763371578b9cf7472
Châteauneufsandrine <chateauneufsandrine188@gmail.com>
2026-03-26 11:04:33 +0100

    add exercice3

--- exercice3/index.php
+++ exercice3/index.php
@@ -1,3 +1,10 @@
 <!-- EXERCICE3//////////////////////////////////////////////////////////////////////////////////
 Faire une page index.php. Tester sur cette page que tous les paramètres de cette URL 
 existent et les afficher: index.php?startDate=2/05/2016&endDate=27/11/2016 -->
+
+<?php 
+
+var_dump($_GET);
+exit();
+
+?>
