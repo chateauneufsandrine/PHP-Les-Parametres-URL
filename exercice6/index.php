939e58e26d827c64e1885e871ee771072ccaec3f
Châteauneufsandrine <chateauneufsandrine188@gmail.com>
2026-03-26 11:09:08 +0100

    add exercice6

--- exercice6/index.php
+++ exercice6/index.php
@@ -1,3 +1,11 @@
 <!-- EXERCICE6/////////////////////////////////////////////////////////////////////////////////
 Faire une page index.php. Tester sur cette page que tous les paramètres de cette URL 
 existent et les afficher: index.php?building=12&room=101 -->
+
+
+<?php 
+
+var_dump($_GET);
+exit();
+
+?>
