e4b989c203216e1c6a4469192c12d7889b0a7857
Châteauneufsandrine <chateauneufsandrine188@gmail.com>
2026-03-26 11:34:57 +0100

    add2 exercice6

--- exercice6/index.php
+++ exercice6/index.php
@@ -6,6 +6,19 @@ existent et les afficher: index.php?building=12&room=101 -->
 <?php 
 
 var_dump($_GET);
-exit();
+
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="index.php?building=12&room=101">Accueil</a>
+</body>
+</html>
